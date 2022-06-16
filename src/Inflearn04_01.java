b1735baa43786470f0a8d68167e703064d8a3bbe
tjdud3550 <tjdud3550@likelion.org>
2022-06-17 00:24:18 +0900

    HashMap

--- src/Inflearn04_01.java
+++ src/Inflearn04_01.java
@@ -1,9 +1,39 @@
-
+import java.util.*;
 public class Inflearn04_01 {
+	
+	public char solution(int n, String s) {
+		char answer= ' ';
+		HashMap<Character, Integer> map = new HashMap<>();
+		for(char x : s.toCharArray()) {
+			map.put(x, map.getOrDefault(x,0)+1);
+		}
+		
+		int max = Integer.MIN_VALUE;
+		for(char key : map.keySet()) {
+			//System.out.println(x+""+map.get(x));
+			if(map.get(key)>max) {
+				max = map.get(key);
+				answer = key;
+			}
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		return answer;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Inflearn04_01 T = new Inflearn04_01();
+		Scanner sc= new Scanner(System.in);
+		int n = sc.nextInt();
+		String str = sc.next();
+		System.out.println(T.solution(n, str));
 	}
 
 }
