fcf5b3bd162c5e79d613a4a171f815723abcea6a
tjdud3550 <tjdud3550@likelion.org>
2022-06-17 15:37:40 +0900

    HashMap

--- src/Inflearn04_05.java
+++ src/Inflearn04_05.java
@@ -1,9 +1,54 @@
+import java.util.*;
 
+//set 중복제거 Treeset 정렬과 중복제거 가능~!
 
 public class Inflearn04_05 {
+	public int solution(int n , int k, int[]arr) {
+		int answer=-1;
+		TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());	
+		
+		//3중 for문
+		for(int i=0; i<n; i++){
+			for(int j=i+1; j<n; j++){
+				for(int l=j+1; l<n; l++){
+					Tset.add(arr[i]+arr[j]+arr[l]);
+				}
+			}
+		}
+		
+		int cnt=0;
+		//Tset.remove(143);
+		//System.out.println(Tset.size());
+		
+		for(int x : Tset){
+			//System.out.println(x);
+			//System.out.println("first : "+ Tset.first());
+			//System.out.println("last : "+ Tset.last());
+
+			cnt++;
+			if(cnt==k) return x;
+		}		
+
+				
+		
+	return answer;
+	
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Inflearn04_05 T = new Inflearn04_05();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		
+		int [ ] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		
+		System.out.println(T.solution(n, k, arr));
 
 	}
 
