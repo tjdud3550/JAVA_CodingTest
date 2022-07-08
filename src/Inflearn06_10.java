d1b21046b322c1a638ca451ad44b1ce1c61c8b29
tjdud3550 <tjdud3550@likelion.org>
2022-07-08 16:13:16 +0900

    DFS

---
+++ src/Inflearn06_10.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Inflearn06_10 {
+	public int count(int[] arr, int capacity) {
+		int cnt =1, sum =0;
+		for(int x: arr) {
+			if(sum+x>capacity) {
+				cnt++;
+				sum=x;
+			}
+			else sum+=x;
+		}
+	
+		return cnt;
+		
+		
+	}
+	public int solution(int n , int m , int[] arr) {
+		int answer = 0;
+		int lt = Arrays.stream(arr).max().getAsInt();
+		int rt = Arrays.stream(arr).sum();
+		while(lt<=rt) {
+			int mid = (lt+rt)/2;
+			if(count(arr,mid)<=m) {
+				answer=mid;
+				rt=mid-1;
+				
+			}
+			else lt=mid+1;
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Inflearn06_10 T = new Inflearn06_10();
+	Scanner sc =new Scanner(System.in);
+	int n = sc.nextInt();
+	int m = sc.nextInt();
+	int[] arr = new int[n];
+	for(int i = 0; i<n; i++) {
+		arr[i] = sc.nextInt();
+		System.out.println(T.solution(n,m,arr));
+	}
+
+	}
+
+}
